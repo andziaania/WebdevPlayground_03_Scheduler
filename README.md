244274cb599a4e8b5f026323b70924c16be7ed76
andziaania <andziaania@gmail.com>
2019-04-01 21:09:35 +0200

    Events position count in js

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 - [x] **displaying elements basing on ul li**
 - [x] **css positioning.** flex, ::after
 - [x] **attention to proper bahaviour when screen resizing and in mobile view**
+- [x] html5 custom attributes
 
 ### Inspiration and knowledge:
 * recreating https://codyhouse.co/gem/schedule-template
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Import stylesheets
 import './style.css';
 
-(function generateView(){
+(function generateSchedulerRows() {
 
   let timelineUl = document.querySelector('#timeline>ul');
   const START_H = 9;
@@ -17,4 +17,40 @@ import './style.css';
   }
 
   console.log(timelineUl);
-})()
+})();
+
+(function displayEvents() {
+
+  let getMinutestamp = function(hourString) {
+    const pattern = /[0-2]\d:[0-5]\d/;
+    const hourPattern = /^[0-2]\d/;
+    const minutePattern = /[0-5]\d$/;
+    const MINUTES_IN_H = 60;
+    if (!pattern.test(hourString)) {
+      console.log(`getMinutestamp error: ${hourString} doesn't match ${pattern} pattern!`);
+      return;
+    }    
+
+    let h = parseInt(hourString.match(hourPattern));
+    let min = parseInt(hourString.match(minutePattern));
+    return h * MINUTES_IN_H + min;
+  }
+
+  const dayStart = document.getElementsByClassName('timeline-row')[0];
+  const dayStartTime = getMinutestamp(dayStart.innerText);
+  const rowDuration = 60; // one timesheet row is 60 minutes
+  const rowHeight = dayStart.offsetHeight;
+
+  const events = document.getElementsByClassName('event');
+  for (let event of events) {
+    let startTime = getMinutestamp(event.getAttribute('data-start-time'));
+    let endTime = getMinutestamp(event.getAttribute('data-end-time'));
+    let startPosition = (startTime - dayStartTime)/rowDuration * rowHeight;
+    let eventHeight = (endTime - startTime)/rowDuration * rowHeight;
+
+    event.style.height = eventHeight + 'px';
+    event.style.top = startPosition + 'px';
+  }
+
+
+})();
